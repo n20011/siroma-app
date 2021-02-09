07d437778857a6fed0747b3c3d835509e47c4591
shiroma ituki <n20011@std.it-college.ac.jp>
2021-02-09 08:01:52 +0000

    YesNo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Exchange extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { // state初期化
+      isLoaded: false,
+      items: []
+    }
+  }
+  componentDidMount () { // render直後に行いたい処理を書くところ
+    fetch('https://yesno.wtf/api') // api
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.rates)
+        this.setState({
+          isLoaded: true,
+          items: json.answer,
+          image: json.image
+        })
+      })
+  }
+
+  render () {
+    var { items, isLoaded, image } = this.state
+console.log(image)
+    if (!isLoaded) {
+      return <div>...Loading</div>
+    } else {
+      return (
+        <div>
+          <h1 Style='text-align:center'>{items}!</h1>
+          <img src={image} width='100%' height='500' />
+          <button type='button' onclick=''>reload</button>
+        </div>
+      )
+    }
+  }
 }
 
-export default App;
+export default Exchange
+
+render(<Exchange />, document.getElementById('root'))
