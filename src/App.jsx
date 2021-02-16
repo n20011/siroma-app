c81d0f0d3ae51175fc616e6661f99eea8c246679
shiroma ituki <n20011@std.it-college.ac.jp>
2021-02-16 07:36:42 +0000

    design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import './App.css'
+import Button from '@material-ui/core/Button'
 
 class Exchange extends Component {
   constructor (props) {
     super(props)
-    this.state = { // state初期化
+    this.state = {
       isLoaded: false,
       items: []
     }
   }
-  componentDidMount () { // render直後に行いたい処理を書くところ
-    fetch('https://yesno.wtf/api') // api
+  componentDidMount () {
+    fetch('https://yesno.wtf/api')
       .then(res => res.json())
       .then(json => {
         console.log(json.rates)
@@ -24,16 +25,18 @@ class Exchange extends Component {
   }
 
   render () {
-    var { items, isLoaded, images } = this.state
+    const { items, isLoaded, images } = this.state
 console.log(this.state)
     if (!isLoaded) {
-      return <div>...Loading</div>
+      return <h1 Style='text-align:center'>Now Loading...</h1>
     } else {
       return (
-        <div>
-          <h1 Style='text-align:center'>{items}!</h1>
-          <img src={images} width='100%' height='800' />
-          <button Style='width: 100%; height: 60px; font-size: 1.4em' onClick={() => window.location.reload()}>reload</button>
+        <div class='backcolor'>
+          <h1 Style='text-align:center'><font color='#3d5afe'>{items}!</font></h1>
+          <img class='images' src={images} />
+          <div class='button'>
+            <Button size='large' variant='contained' color='primary' onClick={() => window.location.reload()}>Next Yes or No ?</Button>
+          </div>
         </div>
       )
     }
